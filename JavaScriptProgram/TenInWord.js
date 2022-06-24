9ad5e27440167310729d2034d6dada0407c87862
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-24 10:51:50 +0530

    Number in word like Ten hundred

---
+++ JavaScriptProgram/TenInWord.js
@@ -0,0 +1,27 @@
+var number = 1000000;
+if(number == 1)
+{
+    console.log(number +"= One");  
+}
+else if (number == 10) {
+    console.log(number +"= Ten"); 
+} 
+else if (number == 100){
+    console.log(number +"= Hundred");
+}
+else if (number == 1000) {
+    console.log(number +"= Thousand"); 
+} 
+else if (number == 10000){
+    console.log(number +"= Ten Thousand");
+}
+else if (number == 100000) {
+    console.log(number +"= One Lakh"); 
+} 
+else if (number == 1000000){
+    console.log(number +"= Ten Lakh");
+}
+else
+{
+    console.log(number +"= One Crore");
+}
