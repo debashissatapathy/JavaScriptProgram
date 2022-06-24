407eca12600b7e2b1a7a71ca80646c0374d083ef
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-24 10:36:33 +0530

    Print number to word

---
+++ JavaScriptProgram/NumberToWord.js
@@ -0,0 +1,34 @@
+//const prompt = require("prompt-sync")();
+var number = 3 //prompt("Please enter a single digit number :");
+if(number == 0)
+{
+    console.log(number +"= Zero");  
+}
+else if (number == 1) {
+    console.log(number +"= one"); 
+} 
+else if (number == 2){
+    console.log(number +"= Two");
+}
+else if (number == 3) {
+    console.log(number +"= Three"); 
+} 
+else if (number == 4){
+    console.log(number +"= Four");
+}
+else if (number == 5) {
+    console.log(number +"= Five"); 
+} 
+else if (number == 6){
+    console.log(number +"= Six");
+}
+else if (number == 7) {
+    console.log(number +"= Seven"); 
+} 
+else if (number == 8){
+    console.log(number +"= Eight");
+}
+else {
+    console.log(number +"= Nine"); 
+} 
+
